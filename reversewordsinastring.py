2c8fe8a668e4bba0d5ca6b813cfff40650ae9280
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2026-03-09 17:52:57 -0400

    beats 100% for runtime

---
+++ reversewordsinastring.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        #iterate through the string , while current character not equal to " " add to a word array
+        #if current character == " " make word array into a string and push onto stack with the space
+        #at end of the list pop until empty , 
+        # arr = s.split()
+        # ans=[]
+        # for i in range(len(arr)):
+        #     if i != len(arr)-1:
+        #         ans.append(arr[i])
+        #         ans.append(" ")
+        #     else:
+        #         ans.append(arr[i])
+        # ans.reverse()
+        # return "".join(ans)
+
+        ##alternate version one liner
+        return " ".join(s.split()[::-1])

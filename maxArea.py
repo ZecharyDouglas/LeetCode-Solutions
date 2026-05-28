f9b514d07d2c4d24a7d4a519b8fa6bfb2e122b4e
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2026-05-27 22:04:49 -0400

    beats 85% #complete the thought #what is true right now?

---
+++ maxArea.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left=0
+        right=len(height)-1
+        ans=0
+        while left < right:
+            width = right - left
+            length = min(height[left] , height[right])
+            area = length * width
+            if area > ans:
+                ans=area
+            if height[left] < height[right]:
+                left+=1
+            else:
+                right-=1
+        return ans
